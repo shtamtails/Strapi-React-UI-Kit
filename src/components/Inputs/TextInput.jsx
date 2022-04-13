4dcb6ea34854cc6ec1fc68f8ca19ff876ea31b12
shtamtails <maximyudin1@gmail.com>
2022-04-14 00:52:06 +0300

    Added TextInput component

--- src/components/Inputs/TextInput.jsx
+++ src/components/Inputs/TextInput.jsx
@@ -1,54 +1,77 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
-import { AiOutlineLoading, AiFillApple } from "react-icons/ai";
-
-// ! Props:
-// ? disabled
-// ? required
-// ? label
-// ? description
-// ? placeholder
-// ? loading
-
-// TODO Add red border to input and description when error handled
+import { AiOutlineLoading } from "react-icons/ai";
 
 export const TextInput = ({ value, setValue, size, ...props }) => {
   if (props.loading === true) {
     props.disabled = true;
   }
-  const handleSubmit = () => {
-    if (props.required && value.length === 0) {
-      console.log("error");
-    }
-  };
+
   return (
-    <>
-      <div className="text-input-container">
-        <div className={`${props.required ? "required " : ""}input-label`}>{props.label}</div>
-        <div className="text-input">
-          <input
-            required={props.required && true}
-            disabled={props.disabled && true}
-            type="text"
-            className={`${props.disabled ? "input-disabled" : ""}`}
-            placeholder={props.placeholder && props.placeholder}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-            onSubmit={handleSubmit}
-          />
-          {props.loading && (
-            <div className="input-loading">
-              <AiOutlineLoading />
-            </div>
-          )}
-        </div>
-        <div className="input-description">{props.description}</div>
+    <div className="text-input-container">
+      <div className={`${props.required ? "required " : ""}input-label`}>{props.label}</div>
+      <div className="text-input">
+        <input
+          required={props.required && true}
+          disabled={props.disabled && true}
+          type="text"
+          className={`${props.disabled ? "input-disabled" : ""}`}
+          placeholder={props.placeholder && props.placeholder}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        {props.loading && (
+          <div className="input-loading">
+            <AiOutlineLoading />
+          </div>
+        )}
       </div>
-    </>
+      <div className="input-description">{props.description}</div>
+    </div>
   );
+
+  // ! Form to test submit
+
+  // return (
+  //   <>
+  //     <form action="submit">
+  //       <div className="text-input-container">
+  //         <div className={`${props.required ? "required " : ""}input-label`}>{props.label}</div>
+  //         <div className="text-input">
+  //           <input
+  //             required={props.required && true}
+  //             disabled={props.disabled && true}
+  //             type="text"
+  //             className={`${props.disabled ? "input-disabled" : ""}`}
+  //             placeholder={props.placeholder && props.placeholder}
+  //             onChange={(e) => {
+  //               setValue(e.target.value);
+  //             }}
+  //           />
+  //           {props.loading && (
+  //             <div className="input-loading">
+  //               <AiOutlineLoading />
+  //             </div>
+  //           )}
+  //         </div>
+  //         <div className="input-description">{props.description}</div>
+  //       </div>
+  //       <button type="submit"></button>
+  //     </form>
+  //   </>
+  // );
 };
 
-TextInput.propTypes = {};
+TextInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  required: PropTypes.bool,
+  loading: PropTypes.bool,
+  label: PropTypes.string,
+  description: PropTypes.string,
+  placeholder: PropTypes.string,
+};
 
 export default TextInput;
